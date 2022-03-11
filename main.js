87ae1fa16f347695a00789c8986fab684472445d
celebrater <tkgm45nbchk@gmail.com>
2022-03-11 15:47:27 +0900

    added the case of array and object

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ const storage = localStorage;
 storage.setItem('test', 'shirogane');
 storage.test2 = 'hideru';
 storage['test3'] = false; // string
+storage.test4 = ["hoge", "fuga"];
+storage.test5 = { hoge: "fuga" };
+storage.test6 =  JSON.stringify({ hoge: "fuga" });
 
 /**
  * 値の取り出し方
@@ -18,6 +21,10 @@ console.log(storage.test2);
 console.log(storage['test3']);
 console.log(typeof storage.test3);
 console.log(storage.none); // undefined
+console.log(storage.test4); // 配列は文字列として保管される
+console.log(storage.test5); // object は JSON 変換が必要
+console.log(JSON.parse(storage.test6)); // オブジェクトはこう読み込む
+
 
 const one = document.getElementById("one");
 const two = document.getElementById("two");
